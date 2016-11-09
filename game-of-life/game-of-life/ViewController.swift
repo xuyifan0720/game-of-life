5694c56560bb16b73ee6411659c2afcab1ea1e1e
Yifan Xu <xuyifan0720@gmail.com>
2016-11-09 13:46:07 -0500

    initial commit

---
+++ game-of-life/game-of-life/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  game-of-life
+//
+//  Created by Yifan Xu on 11/9/16.
+//  Copyright © 2016 Yifan&Hannah_production. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
